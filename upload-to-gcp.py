0b3723a33ba37b5205fb4af4c60f65ba378be201
Suhailhassanbhat <shb2145@columbia.edu>
2025-01-22 17:02:20 -0500

    gcp uploader

---
+++ upload-to-gcp.py
@@ -0,0 +1,28 @@
+# %%
+from google.cloud import storage
+import os
+# Function to upload file to Google Cloud Storage
+def upload_to_gcs(local_file_path, bucket_name, gcs_file_path):
+    # Initialize a Cloud Storage client
+    storage_client = storage.Client()
+    # Reference to the bucket
+    bucket = storage_client.bucket(bucket_name)
+    # Upload the file
+    blob = bucket.blob(gcs_file_path)
+    blob.upload_from_filename(local_file_path)
+    print(f"File {local_file_path} uploaded to {gcs_file_path}.")
+
+# Upload the CSV to Google Cloud Storage
+local_file_path = 'scraped_whitehouse_posts.csv'
+bucket_name = 'your-life-in-data'
+gcs_file_path = "executive-orders/scraped_whitehouse_posts.csv"
+
+# Upload the file to GCS
+upload_to_gcs(local_file_path, bucket_name, gcs_file_path)
+
+print("Scraping complete. New data saved locally and uploaded to Google Cloud Storage.")
+
+# %%
+
+
+
